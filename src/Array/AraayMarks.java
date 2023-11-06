6315a1507202395f9b298bb8de561d401f74b19a
Rajesh10 <guddewadi10@gmail.com>
2023-11-06 11:42:47 +0530

    added

---
+++ src/Array/AraayMarks.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class AraayMarks {
+
+	public static void main(String[] args) {
+		int marks[]= new int[3];
+		
+		marks[0]=97;
+		marks[1]=92;
+		marks[2]=95;
+		
+		for(int i=0;i<3; i++) {
+			System.out.println(marks[i]);
+		}
+
+	}
+
+}
